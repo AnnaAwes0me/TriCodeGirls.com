9c9a0aec707462f4122752795a879c06b4cc1fac
Jakob Lorberblatt <jjlorberblatt@gmail.com>
2019-03-20 23:07:57 -0400

    Updated the page to reflect php based content

---
+++ tricodegirls.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Tri Code Girls - Homepage</title>
+<link rel="stylesheet" type="text/css" href="tricodegirls.css">
+</head>
+<div class="bigDiv">
+	<body>
+	<div class="center">
+		<div class="menu">
+			<div class="link1">
+			<a href="www.tricodegirls.com">Home Page</a>
+			</div>
+			<div class="link2">
+			<a href="/highschool.html">Highschool Group</a>
+			</div>
+			<div class="link3">
+			<a href="/middleschool.html">Middle School Group</a>
+			</div>
+			<div class="link4">
+			<a href="/cyberstart.html">Girls Go Cyberstart</a>
+			</div>
+		</div>
+		<h1>Welcome to Tri Code Girls!</h1>
+		<div class="textBody">
+			<?php
+				$vname = $_GET['page'];
+				if (!$vname) {
+					$vname = 'tricodegirls';
+				}
+				$content = readfile($vname . '.html');
+				echo($content);
+			?>
+		</div>
+	</div>
+	</body>
+</div>
+</html>
