19a2cb19c205b0f93ff431c4b8e09b8ed0c92e4a
Zaia <zanyzaia@gmail.com>
2020-06-26 18:46:29 -0400

    Ready to push

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	<div class="center">
 		<div class="menu">
 		<div class="link1">
-			<a href="/tricodegirls.php?page=tricodegirls.html">Home Page</a>
+			<a href="/tricodegirls.html">Home Page</a>
 			</div>
                 	<div class="link2">
 			<a href="/highschool.html">Highschool Group</a>
